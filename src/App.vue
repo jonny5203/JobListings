f7ea99c236750db1cc2335c08afce3b0916ab327
jonny5203 <jonnyanhvu@gmail.com>
2023-04-22 00:58:01 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="ChangeName('Zelda')">Change Name</button>
+    <button @click="ChangeAge(18)">Change Age</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, reactive, ref, toRef } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: {},
+    setup() {
+      const name = ref('Link');
+      const age = ref<string | number>(17);
+
+      return {
+        name,
+        age
+      }
+    },
+    methods: {
+      ChangeName(name: string){
+        this.name = name;
+      },
+      ChangeAge(age: number | string){
+        this.age = age;
+      }
+    }
+  });
+</script>
+
+<style>
+</style>
